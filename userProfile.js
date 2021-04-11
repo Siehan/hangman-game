21dbc4bd4e04f9e97d8f48c7b52549d93925827a
Siehan <memain.ye.sylvie@gmail.com>
2021-04-11 18:13:13 +0200

    done

--- userProfile.js
+++ userProfile.js
@@ -1,9 +1,20 @@
 const chalk = require("chalk");
 const readlineSync = require("readline-sync");
-const fs = require("fs");
-const readline = require("readline");
 
-console.log(chalk.yellow("😈😈😈😈😈😈😈😈 THE HANGMAN 😈😈😈😈😈😈😈😈"));
+console.log(
+  chalk.yellow(`
+
+████████ ██   ██ ███████     ██   ██  █████  ███    ██  ██████  ███    ███  █████  ███    ██ 
+   ██    ██   ██ ██          ██   ██ ██   ██ ████   ██ ██       ████  ████ ██   ██ ████   ██ 
+   ██    ███████ █████       ███████ ███████ ██ ██  ██ ██   ███ ██ ████ ██ ███████ ██ ██  ██ 
+   ██    ██   ██ ██          ██   ██ ██   ██ ██  ██ ██ ██    ██ ██  ██  ██ ██   ██ ██  ██ ██ 
+   ██    ██   ██ ███████     ██   ██ ██   ██ ██   ████  ██████  ██      ██ ██   ██ ██   ████ 
+
+`)
+);
+
+//console.log(chalk.yellow("😈😈😈😈😈😈😈😈 THE HANGMAN 😈😈😈😈😈😈😈😈"));
+
 console.log("");
 
 userFirstName = readlineSync.question(chalk.green("Hello, may I have your first name? : "));
@@ -11,10 +22,14 @@ userLastName = readlineSync.question(chalk.green("And your family name? : "));
 userAge = readlineSync.question(chalk.green(`What is your age ${userFirstName} ? : `));
 gameStart = readlineSync.question(chalk.green("Thanks  ↩ "));
 
+console.log("");
+
 if (readlineSync.keyInYNStrict(chalk.yellow("Well, so are you ready to play at the Hangman ?"))) {
   // 'Y' key was pressed.
-  console.log(chalk.red(`Let start the ÷÷÷÷ Hangman Game ÷÷÷÷ ${userFirstName} 😈 !`));
+  console.log("");
+  console.log(chalk.yellow(`Let start the ÷÷ HANGMAN GAME ÷÷ ${userFirstName} 😈 !`));
   // process.exit(0); // Si pas de process.exist revient sur Thanks
+  console.log("");
 } else {
   // 'N' key was pressed.
   console.log(chalk.green(`See you soon ${userFirstName}!`));
